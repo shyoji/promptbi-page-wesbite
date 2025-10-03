bb67580da0a35461fccdbfd03da542e2df1699ae
shyoji <shyoji@skizaa.com>
2025-10-03 11:38:26 +0530

    Fix: add supabase url

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = "https://lnyxyggjwkekqzukzjrk.supabase.co";
-const supabaseAnonKey = "1234";
+
+const supabaseAnonKey="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxueXh5Z2dqd2tla3F6dWt6anJrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkzNDIyNzEsImV4cCI6MjA3NDkxODI3MX0.GP1FQB606x9el93GEVn2bYG6Px4eVwaDNCSi-fOItNE"
+
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
